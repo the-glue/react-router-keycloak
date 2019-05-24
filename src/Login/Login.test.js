b9bd48bb25b7a311b3015c8b49e0870b49a69fec
wouterwouters <wouter.wouters@thegue.com>
2019-05-24 13:12:48 +0200

    change naming snapshot

--- src/Login/Login.test.js
+++ src/Login/Login.test.js
@@ -1,18 +1,53 @@
 import React from 'react';
+import { MemoryRouter, Route } from 'react-router-dom';
 import renderer from 'react-test-renderer';
 import { Login } from '../index';
-import { configureKeycloak } from '../keycloak/keycloak';
+import { configureKeycloak, getKeycloak } from '../keycloak/keycloak';
+
+jest.mock('../keycloak/keycloak');
 
 describe('Login', () => {
   beforeAll(() => {
     configureKeycloak('dummy url', 'dummy realm', 'dummy id');
   });
+
   it('renders without crashing given the required props', () => {
+    const keycloak = {
+      authenticated: false,
+      token: 'token'
+    };
+    getKeycloak.mockImplementation(() => Promise.resolve(keycloak));
     const props = {
       onSuccess: jest.fn(),
       onFailure: jest.fn()
     };
-    const wrapper = renderer.create(<Login {...props} />).toJSON();
+    const wrapper = renderer
+      .create(
+        <MemoryRouter initialEntries={['/login']}>
+          {' '}
+          <Route path="/login" render={() => <Login {...props} />} />
+        </MemoryRouter>
+      )
+      .toJSON();
     expect(wrapper).toMatchSnapshot();
   });
+
+  /*it('call onSuccess', () => {
+    const props = {
+      onSuccess: jest.fn(),
+      onFailure: jest.fn()
+    };
+    const p = Promise.resolve(props.onSuccess);
+    const keycloak = {
+      init: jest.fn(() => p),
+      authenticated: false,
+      token: "token"
+    }
+    
+    getKeycloak.mockImplementation(() => Promise.resolve(keycloak));
+    
+    const wrapper = renderer.create(<MemoryRouter initialEntries={['/login']}> <Route path="/login" render={() => <Login {...props} />}/></MemoryRouter>).toJSON();
+    
+    expect(props.onSuccess).toHaveBeenCalled();
+  });*/
 });
--- src/Logout/Logout.test.js
+++ src/Logout/Logout.test.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { MemoryRouter, Route } from 'react-router-dom';
 import renderer from 'react-test-renderer';
 import { Logout } from '../index';
-import { configureKeycloak } from '../keycloak/keycloak';
+import { configureKeycloak, getKeycloak } from '../keycloak/keycloak';
 
 describe('Logout', () => {
   beforeAll(() => {
@@ -22,5 +22,21 @@ describe('Logout', () => {
       )
       .toJSON();
     expect(wrapper).toMatchSnapshot();
+  });
+  it('onSuccess should be called when logging out and keycloak is available', () => {
+    const props = {
+      redirectTo: '/dummy',
+      onSuccess: jest.fn(),
+      onFailure: jest.fn()
+    };
+
+    const wrapper = renderer
+      .create(
+        <MemoryRouter initialEntries={['/logout']}>
+          <Route path="/logout" render={() => <Logout {...props} />} />
+        </MemoryRouter>
+      )
+      .toJSON();
+    expect(props.onSuccess).toHaveBeenCalled();
   });
 });
